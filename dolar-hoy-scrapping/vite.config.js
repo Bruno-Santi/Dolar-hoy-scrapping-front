af43697d06353d39718bcc340dcf069c1ee7a64b
Bruno Santimaria <brun0.23@outlook.com>
2023-05-22 21:16:10 -0300

    Update vite.config.js

--- dolar-hoy-scrapping/vite.config.js
+++ dolar-hoy-scrapping/vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    // include: ['linked-dep'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
+    },
+  },
 });
